80f2912777433d2e1b5e30794a6d062e5bdc3062
Julian <jutanke@googlemail.com>
2014-11-06 14:03:24 +0100

    start with LPBCast

---
+++ client/lpbCast.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Julian on 11/6/2014.
+ * http://perso.telecom-paristech.fr/~kuznetso/pubs/Lpbcast_tocs.pdf
+ */
+(function(Gossip){
+
+
+
+})(typeof window.Gossip === 'undefined'?
+    window.Gossip = {} : window.Gossip);
