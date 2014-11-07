bafbdc12264771e4c53bce758f859bc152fe3236
Julian <jutanke@googlemail.com>
2014-11-07 15:10:03 +0100

    bootstrapping..

--- server/node.js
+++ server/node.js
@@ -1,5 +1,5 @@
 /**
  * Created by julian on 17/10/14.
  */
-var GossipBroker = require("./gossipjs").Broker({port:9000, debug:true, bootstrapPort:90001});
+var GossipBroker = require("./gossipjs").Broker({port:9000, debug:true, bootstrapPort:9001});
 //GossipBroker.debug('hallo');
