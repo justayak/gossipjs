5ce886f3da37148109a208e5694aaf41ad20b3a8
Julian <jutanke@googlemail.com>
2014-11-07 15:22:02 +0100

    change to client side only

--- server/gossipbroker.js
+++
@@ -1,8 +0,0 @@
-/*
- *  Broker
- */
-var PeerServer = require('peer').PeerServer;
-var server = new PeerServer({port:9000, path:"/b"});
-server.on('connection', function(id){
-    console.log(id);
-});
--- server/node.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by julian on 17/10/14.
- */
-var GossipBroker = require("./gossipjs").Broker({port:9000, debug:true, bootstrapPort:9001});
-//GossipBroker.debug('hallo');
